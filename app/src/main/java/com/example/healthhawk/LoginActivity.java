b96bd499bfaa0447655accfa42704b9e1d8f0555
koll2290 <koll2290@mylaurier.ca>
2021-12-08 23:28:35 -0500

    Added Fake Loading Screen Lol

--- app/src/main/java/com/example/healthhawk/LoginActivity.java
+++ app/src/main/java/com/example/healthhawk/LoginActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
